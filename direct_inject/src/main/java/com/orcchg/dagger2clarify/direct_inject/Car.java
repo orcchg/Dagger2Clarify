29f2f4cf54e52f1a5174713b11e0766e7a4c2222
Maxim Alov <maxim@aludio.com>
2017-05-25 12:35:07 +0300

    DirectInject module compiles

---
+++ direct_inject/src/main/java/com/orcchg/dagger2clarify/direct_inject/Car.java
@@ -0,0 +1,8 @@
+package com.orcchg.dagger2clarify.direct_inject;
+
+import javax.inject.Inject;
+
+public class Car {
+
+    @Inject Engine engine;
+}
---
+++ direct_inject/src/main/java/com/orcchg/dagger2clarify/direct_inject/Engine.java
@@ -0,0 +1,4 @@
+package com.orcchg.dagger2clarify.direct_inject;
+
+public class Engine {
+}
